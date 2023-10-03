cd7b1e2c0cce45f422d899522a4c66b3ae46a9b9
WeraChernevich <138397007+WeraChernevich@users.noreply.github.com>
2023-10-03 23:20:07 +0300

    Effer
    test

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-    effect: "cube",
-    cubeEffect: {                       // added
-        slideShadows: true,             // added（Presence of shadows on the slide surface
-        shadow: true,                   // added（Presence of shadows below the slide
-        shadowOffset: 10,              // added（Position of shadows below the slide (in pixels)
-        shadowScale: 1,                 // added（Size Ratio of shadows below the slide (0~1)
-    },                                  // added
-    grabCursor: true,
-    speed: 1500,
-    autoplay: {
-        delay: 2000,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
     },
-
 
     // If we need pagination
     pagination: {
